f47b2bec2887132fd5e8b66aa7ebc1573211474e
Mathis Loussert <mathis.loussert@etu.univ-grenoble-alpes.fr>
2025-03-26 09:35:14 +0100

    classe Selection

--- projet_devops/src/main/java/projet_devops/Selection.java
+++ projet_devops/src/main/java/projet_devops/Selection.java
@@ -1,13 +1,40 @@
 package projet_devops;
 
+import java.util.ArrayList;
+
 public class Selection {
     
+    @SuppressWarnings("unchecked")
 
-    public DataFrame selectIndex(DataFrame df, int start, int end) {
+    public <T> DataFrame selectIndex(DataFrame df, int start, int end) {
         DataFrame newDf = new DataFrame();
-        for (int i = start; i < end; i++) {
+
+        for (String columnName : df.getColumnNames()) {
+
+            DataColumn<T> originalColumn = (DataColumn<T>) df.getColumn(columnName);
+            DataColumn<T> indexColumn = new DataColumn<>(columnName, originalColumn.getDataType());
             
-        
+            for (int i = start; i <= end; i++) {
+                T value = originalColumn.get(i);
+                indexColumn.add(value);
+            }
+
+            newDf.addColumn(columnName, indexColumn);
+        }
+
+        return newDf;
+    }  
+
+
+    @SuppressWarnings("unchecked")
+
+    public <T> DataFrame selectColumn(DataFrame df, ArrayList<String> columns) {
+        DataFrame newDf = new DataFrame();
+
+        for (int i = 0; i < columns.size(); i++) {
+                DataColumn<T> originalColumn = (DataColumn<T>) df.getColumn(columns.get(i));
+                
+                newDf.addColumn(columns.get(i), originalColumn);
         }
 
         return newDf;
