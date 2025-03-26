694a1695d4d83698e253e9053b8ca3696c30b777
TrRollet <tristan.rollet38@gmail.com>
2025-03-26 09:09:24 +0100

    Correction de la vérification de types, modification du Main et ajout d'un fichier CSV pour tester Prochain commit: les tests JUnit

--- projet_devops/src/main/java/projet_devops/Main.java
+++ projet_devops/src/main/java/projet_devops/Main.java
@@ -1,13 +1,30 @@
 package projet_devops;
 
-/**
- * Hello world!
- *
- */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            if (args.length < 1) {
+                System.err.println("Erreur: Veuillez spécifier le chemin du fichier CSV");
+                System.exit(1);
+            }
+
+            DataFrame df = DataFrame.fromCSV(args[0]);
+
+            System.out.println("DataFrame chargé avec succès !");
+            System.out.println("Nombre de lignes : " + df.getRowCount());
+            System.out.println("Nombre de colonnes : " + df.getColumnCount());
+            System.out.println("\nNom et type de chaque colonne :");
+
+            for (int i = 0; i < df.getColumnCount(); i++) {
+                DataColumn<?> column = df.getColumn(i);
+                System.out.println("Colonne " + (i + 1) + ": " + column.getColumnName() + " (" + column.getDataType().getSimpleName() + ")");
+            }
+
+        } catch (IOException e) {
+            System.err.println("Erreur lors de la lecture du fichier : " + e.getMessage());
+            System.exit(1);
+        }
     }
 }
